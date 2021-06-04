7539540b94f13f85a3ae19137adb8801a973c622
MarcosVs98 <marcosvs@protonmail.com>
2021-06-04 11:42:15 -0300

    Correções na estrutura do proxy list client

--- __init__.py
+++ __init__.py
@@ -75,7 +75,6 @@ class ProxyListAPI():
 		pass
 
 	def proxy_ordenate(self, proxy_item, reference, name=None):
-
 		if not reference in self._result:
 			self._result[reference] = []
 		else:
@@ -89,19 +88,19 @@ class ProxyListAPI():
 		self._result['info'] = {}
 		for proxy_line in proxies_list:
 			proxy_item = self.parse(proxy_line)
-			# ordena por tipo de schema
+			# sort by schema type
 			self.proxy_ordenate(proxy_item, f'scheme-{proxy_item.proxy_scheme}')
-			# ordena por nivel de anonimato
+			# sort by anonymity level
 			self.proxy_ordenate(proxy_item, f'level_anonymity-{proxy_item.proxy_level_anonymity}')
-			# ordena por pais origem
+			# sort by country of origin
 			self.proxy_ordenate(proxy_item, f'country-{proxy_item.proxy_country}')
-			# ordena por porta
+			# sorts by http port
 			self.proxy_ordenate(proxy_item, f'port-{proxy_item.proxy_port}')
-			# ordena por google passed
+			# sort by proxy who goes through google
 			self.proxy_ordenate(proxy_item, f'google_passed-{proxy_item.proxy_google_passed}')
 		self._result['info']['header'] = proxies_header
 		self._result['info']['count_proxies'] = len(proxies_list)
-
+		# Prepare a json file for cache
 		with open('proxy-list-cache.json', 'w') as f:
 			json.dump(self._result, f, indent=4)
 
@@ -122,6 +121,8 @@ class ProxyListAPI():
 		|   |_ _ _ _ _    |  | |  |
 		|             |   |  | |  |
 		200.2.125.90:8080 AR-N-S! +
+		└────────────┬────────────┘
+				 Proxy line
 		"""
 		try:
 			proxy_splited = proxy_line.split(' ')
@@ -130,32 +131,12 @@ class ProxyListAPI():
 			proxy_port = proxy_address_info[1]
 			proxy_info = proxy_splited[1].split('-')
 			proxy_country = proxy_info[0]
-			proxy_anonymity = proxy_info[1]
-
-			if proxy_anonymity.startswith('N'):
-				proxy_level_anonymity = 0
-			elif proxy_anonymity.startswith('A'):
-				proxy_level_anonymity = 1
-			elif proxy_anonymity.startswith('H'):
-				proxy_level_anonymity = 2
-			try:
-				proxy_type = proxy_info[2]
-			except IndexError:
-				proxy_type = proxy_info[1]
-			if proxy_type.startswith('S'):
-				proxy_scheme = 'https'
-			else:
-				proxy_scheme = 'http'
-
-			if proxy_type.endswith('!'):
-				proxy_outgoing_ip = True
-			else:
-				proxy_outgoing_ip = False
-
-			if proxy_line.endswith('+'):
-				proxy_google_passed = True
-			else:
-				proxy_google_passed = False
+			proxy_level_anonymity = self._parse_anonymity(proxy_info[1])
+			proxy_type = self._parse_proxy_type(proxy_info)
+			proxy_outgoing_ip = self._parse_output_ip(proxy_type)
+			proxy_scheme = self._parse_http_type(proxy_type)
+			proxy_google_passed = self._parse_google_passed(proxy_line)
+			proxy_uri = f"{proxy_scheme}://{proxy_ip}:{proxy_port}/"
 
 			proxy_parsed = ProxyParsed(
 				proxy_ip=proxy_ip, proxy_port=proxy_port,
@@ -164,10 +145,42 @@ class ProxyListAPI():
 				proxy_country=proxy_country,
 				proxy_google_passed=proxy_google_passed,
 				proxy_outgoing_ip=proxy_outgoing_ip,
-				proxy_uri=f"{proxy_scheme}://{proxy_ip}:{proxy_port}/")
+				proxy_uri=proxy_uri)
 			return proxy_parsed
 		except IndexError as e:
 			raise InvalidProxyToParser(e)
+
+	def _parse_anonymity(self, proxy_info):
+		if proxy_info.startswith('N'):
+			proxy_level_anonymity = 0
+		elif proxy_info.startswith('A'):
+			proxy_level_anonymity = 1
+		elif proxy_info.startswith('H'):
+			proxy_level_anonymity = 2
+		return proxy_level_anonymity
+
+	def _parse_proxy_type(self, proxy_info):
+		try:
+			proxy_type = proxy_info[2]
+		except IndexError:
+			proxy_type = proxy_info[1]
+		return proxy_type
+
+	def _parse_google_passed(self, proxy_line):
+		if proxy_line.endswith('+'):
+			return True
+		return False
+
+	def _parse_output_ip(self, proxy_type):
+		if proxy_type.endswith('!'):
+			return True
+		return False
+
+	def _parse_http_type(self, proxy_type):
+		if proxy_type.startswith('S'):
+			return 'https'
+		return 'http'
+
 
 proxy_api = ProxyListAPI()
 proxy_api.initialize()
